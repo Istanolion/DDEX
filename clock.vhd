4f59cbc0cfb22159d9e8017402bc55fa364abf4f
Istanolion <akuma_diego@hotmail.com>
2018-10-01 11:39:28 -0500

    probar si funciona el muxbcd
    probar si funciona el muxbcd y agregar un debouncing del boton (googleado)

--- clock.vhd
+++ clock.vhd
@@ -3,20 +3,17 @@ USE IEEE.STD_LOGIC_1164.ALL;
 USE IEEE.NUMERIC_STD.ALL;
 ENTITY CLOCK IS
 PORT(
+	clk: in std_logic;
 	S : OUT STD_LOGIC
 );
 END CLOCK;
 ARCHITECTURE BEHAVORIAL OF CLOCK IS
-	signal output: std_logic:='1';
+signal output: std_logic:='0';
 BEGIN
-	process(output) 
-	variable i: integer;
-	begin
-	i:=0;
-	WHILE(i<1000) loop
-		s <= output;
-		output<=not(output);
-		i:=i+1;
-	end loop;
-end process;
+	process(clk) begin
+		if rising_edge(clk) then
+			s<=output;
+			output<=not(output);
+		end if;
+	end process;
 END BEHAVORIAL;
